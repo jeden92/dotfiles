b820121d734ff7d149e89214d35c2a8e50ba665f
Javier Escribano <fesjav@gmail.com>
2017-01-13 21:00:10 +0100

    Configure personal changes

---
+++ brew-apps.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Install apps
+
+brew tap homebrew/completions
+brew install homebrew/completions/docker-completion
+brew install homebrew/completions/docker-compose-completion
+
+brew tap caskroom/cask
+brew cask install alfred
+brew cask install atom
+brew cask install dashlane
+brew cask install data-rescue
+brew cask install docker
+brew cask install dropbox
+brew cask install evernote
+brew cask install figma
+brew cask install firefox
+brew cask install github-desktop
+brew cask install google-chrome
+brew cask install google-drive
+brew cask install iterm2
+brew cask install itsycal
+brew cask install kindle
+brew cask install monolingual
+brew cask install postico
+brew cask install recordit
+brew cask install sequel-pro
+brew cask install sketch
+brew cask install sketch-toolbox
+brew cask install sky-fonts
+brew cask install skype
+brew cask install slack
+brew cask install soulver
+brew cask install spark
+brew cask install spotify
+brew cask install tower
+brew cask install transmit
+brew cask install vlc
+brew cask install wifi-explorer
+
+#tap caskroom/fonts
+#cask font-inconsolata
+#cask font-fira-mono
